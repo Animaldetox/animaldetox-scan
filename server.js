a110027d8304d7ddffb32977fda6bb876ded5ef0
Animaldetox <manonveterinaria@gmail.com>
2026-06-25 12:11:31 +0200

    Clean up server.js by removing package.json data
    Removed package.json content from server.js.

--- server.js
+++ server.js
@@ -23,16 +23,4 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server OK on port " + PORT);
-});
-{
-  "name": "animaldetox",
-  "version": "1.0.0",
-  "main": "server.js",
-  "scripts": {
-    "start": "node server.js"
-  },
-  "dependencies": {
-    "express": "^4.18.2",
-    "cors": "^2.8.5"
-  }
-}
+})
